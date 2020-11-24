10f780551d8bdeec505450565801e6b95edc7fbc
Daquan Zhou <zhoudaquan21@gmail.com>
2020-11-24 09:06:50 +0000

    fix naming

--- mobilenext/mnext_efficient_s.sh
+++ mobilenext/mnext_efficient_s.sh
@@ -4,7 +4,7 @@ shift
 python -m torch.distributed.launch --nproc_per_node=$NUM_PROC train.py "$@" ~/WSNet_improvement/imagenet/ILSVRC/Data/CLS-LOC/ \
     --model mnext_s --num-gpu 1 --lr 0.12 --sched step --decay-rate 0.97  \
     --opt rmsproptf --opt-eps 0.001 --warmup-lr 1e-6 \
-    --decay-epochs 2.4 --epochs 850 \
+    --decay-epochs 2.4 --epochs 450 \
     -b 128 --log-interval 550 -j 64 --smoothing 0.1 \
     --weight-decay 1e-5 --model-ema-decay 0.9999 --model-ema \
     --output ./output/efficienti2rnet_b1_5_68_amp_ra_lr_012_continue --log-interval 500 \
